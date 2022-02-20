895ef7d4593a63aef5149d6d728b3e9c291d802a
David McIntosh <dmcintosh@bblbze.com>
2022-02-20 12:37:21 -0600

    adds navbar + react-hot-toast

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import toast from 'react-hot-toast'
 
 import Loader from "../components/Loader"
 
 export default function Home() {
   return (
     <div>
-      <Loader show />
+      <button onClick={() => toast.success('tank uuuuuuuu! :3')}>
+        Toast Me, please! 🍞
+      </button>
     </div>
   )
 }
