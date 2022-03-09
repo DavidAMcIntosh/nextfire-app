a7c3742e9f051af41fa941a24a468f38e5c1a48b
David McIntosh <dmcintosh@bblbze.com>
2022-03-09 07:27:09 -0600

    adds metatags for seo/bots, adds ssr and isr for post feed, adds custom 404 page.

--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -1,7 +1,68 @@
-export default function PostPage({ }) {
+import styles from '../../styles/Post.module.css';
+import PostContent from '../../components/PostContent';
+import { useDocumentData } from 'react-firebase-hooks/firestore';
+import { firestore, getUserWithUsername, postToJSON } from "../../lib/firebase";
+//import { UserContext } from '@lib/context';
+import { useContext } from 'react';
+
+export async function getStaticProps({ params }) {
+  const { username, slug } = params;
+  console.log(`user: ${username}`)
+  console.log(`slug: ${slug}`)
+  const userDoc = await getUserWithUsername(username);
+  //console.log(`userDoc: ${JSON.stringify(userDoc)}`)
+  let post;
+  let path;
+
+  if (userDoc) {
+    const postRef = userDoc.ref.collection('posts').doc(slug);
+    console.log(`post ref= ${JSON.stringify(postRef)}`);
+    post = postToJSON(await postRef.get());
+
+    path = postRef.path;
+  }
+
+  return {
+    props: { post, path },
+    revalidate: 100,
+  };
+}
+
+export async function getStaticPaths() {
+  const snapshot = await firestore.collectionGroup('posts').get();
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data();
+    return {
+      params: { username, slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: 'blocking',
+  }
+}
+export default function Post(props) {
+  const postRef = firestore.doc(props.path);
+  const [realtimePost] = useDocumentData(postRef);
+
+  const post = realtimePost || props.post;
+
+  //const { user: currentUser } = useContext(UserContext);
+
     return (
-      <main>
-          <h1>Post Page</h1>
+      <main className={styles.container}>
+
+        <section>
+          <PostContent post={post}/>
+        </section>
+
+        <aside className='card'>
+          <p>
+            <strong>{post.heartCount || 0} ❤️</strong>
+          </p>
+        </aside>
+
       </main>
     )
   }
